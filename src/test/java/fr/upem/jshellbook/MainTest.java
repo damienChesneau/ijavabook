ac2d8765784177d28d7d8b97ef8267a7eb834f84
Damien Chesneau <ches.damien@gmail.com>
2015-11-05 21:45:04 +0100

    Initial commit.

---
+++ src/test/java/fr/upem/jshellbook/MainTest.java
@@ -0,0 +1,14 @@
+package fr.upem.jshellbook;
+
+/**
+ * JShell Book program input.
+ *
+ * @author Damien Chesneau - contact@damienchesneau.fr
+ */
+public class MainTest {
+
+
+    public static void main(String[] args) {
+        //Let's play !
+    }
+}
